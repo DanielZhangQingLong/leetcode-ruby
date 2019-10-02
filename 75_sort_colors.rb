2e7c5c3e39f56b319a9f0c8bef39d87c3c5a83fd
daniel <thecndra@gmail.com>
2019-10-02 18:21:27 +0800

    Add 75 solutions

---
+++ 75_sort_colors.rb
@@ -0,0 +1,36 @@
+# Problem Link: https://leetcode.com/problems/sort-colors/
+# @param {Integer[]} nums
+# @return {Void} Do not return anything, modify nums in-place instead.
+# Input: [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+def sort_colors(nums)
+  count = Array.new(3, 0)
+  for i in (0 ... nums.size)
+    puts i
+    count[nums[i]] += 1
+  end
+
+  index = 0
+  for i in (0 ... count[0])
+    nums[index] = 0
+    index += 1
+  end
+
+  for i in (0 ... count[1])
+    nums[index] = 1
+    index += 1
+  end
+
+  for i in (0 ... count[2])
+    nums[index] = 2
+    index += 1
+  end
+  nums.inspect
+end
+
+def test
+   nums = [2,0,2,1,1,0]
+   puts sort_colors nums
+end
+
+test
