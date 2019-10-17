c7eff84aaee73bab806f421a8e5a78cb41aa23f5
daniel <thecndra@gmail.com>
2019-10-17 08:24:18 +0800

    #249 On

--- 249_intersection-of-two-arrays.rb
+++ 249_intersection-of-two-arrays.rb
@@ -1,12 +1,15 @@
 # @param {Integer[]} nums1
 # @param {Integer[]} nums2
 # @return {Integer[]}
-# Tn = On
+# Tn = O(nlogn)
 # Sn = On
 require 'set'
 def intersection(nums1, nums2)
+  #O(nlogn)
   set1 = Set.new(nums1)
+  #O(nlogn)
   res = Set.new
+  #O(n)
   nums2.each do |n|
     res.add n if set1.include? n
   end
